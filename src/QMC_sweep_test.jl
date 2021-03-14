a7fa193dd5f5f80975d51039c6bd85a02044b370
spasmann <46267220+spasmann@users.noreply.github.com>
2021-03-14 16:42:25 -0400

    QMC_sweep_test.jl update
    - Added axes labels and legend to angular flux exit distribution plots.
    - Added description of plots
    - Copied documentation over from QMC_sweep.jl
            - Removed description of "move_part"
            - Expanded definition of QMC_sweep function

--- src/QMC_sweep_test.jl
+++ src/QMC_sweep_test.jl
@@ -1,5 +1,69 @@
 include("QMC_sweep.jl")
 
+###############################################################################
+# Function Documentation
+###############################################################################
+"""
+QMC_Sweep:
+        Performs one QMC sweep of a 1D, 1 group, slab, problem. Particularly 
+        designed for the problems outlined in Garcia et al. where 
+        Sigma_s = exp(-x/c) and the angular flux distribution is tallied at the 
+        boundaries.
+
+    Inputs:
+    -------
+        source_strength:
+            Scalar value, strength of internal source
+        N:
+            Number of internal source particles
+        has_right:
+            True or false, source right boundary
+        has_left:
+            True or false, source left boundary
+        Nb:
+            Number of boundary source particles
+        Nx:
+            Number of scalar flux and current tallying cells
+        Lx:
+            Width of slab (cm)
+        nbins:
+            Number of exit angular flux tally bins
+        dt:
+            Time increment (s). Set dt = Inf if S.S. is desired
+        v:
+            Particle velocity (cm/s)
+        sigt:
+            Scalar value, total macroscopic cross section
+        c:
+            Scattering cross section varies with exp(-x/c)
+        phi_avg:
+            Optional input of cell averaged scalar flux.
+    Outputs:
+    --------
+        midpoints:
+            Array of length Nx, midpoints of cells
+        edges:
+            Array of length Nx+1, cell boundaries
+        phi_avg:
+            Array of length Nx, average scalar flux across each cell
+        phi_edge:
+            Array of length Nx+1, scalar flux at cell boundaries
+        dphi:
+            Spatial derivative of phi_avg
+        J_avg:
+            Array of length Nx, average cell current
+        J_edge:
+            Array of length Nx+1, current at cell boundaries
+        exit_right_bins:
+            Array of length nbins, angular flux distribution at right boundary
+        exit_left_bins:
+            Array of length nbins, angular flux distribution at left boundary
+"""
+
+###############################################################################
+# Example Inputs
+###############################################################################
+
 dt = Inf# Inf #time increment
 v = 1 #velocity (cm/s)
 c = 1 #scattering exp(x/c) term from garica problem
@@ -22,6 +86,17 @@ midpoints,edges, phi_avg,phi_edge,dphi,J_avg,J_edge, exit_right_bins, exit_left_
 ###############################################################################
 # Example Plotting
 ###############################################################################
+"""
+Figure 1: 
+          - cell averaged scalar flux 
+          - cell averaged spatial derivative of scalar flux
+          - cell averaged current 
+
+Figure 2: 
+          - Left boundary angular flux distribution
+          - right boundary angular flux distribution
+
+"""
 
 #cell averaged scalar flux
 figure(figsize = (5,10))
@@ -30,17 +105,21 @@ subplot(311)
 plot(midpoints,phi_avg)
 ylabel("phi")
 xlabel("midpoints")
+title("Cell Averaged Scalar Flux")
 
+# cell Averaged Spatial Derivative of Scalar Flux
 subplot(312)
 plot(midpoints,dphi)
 ylabel("dphi")
 xlabel("midpoints")
+title("Cell Averaged Spatial Derivative of Scalar Flux")
 
+# cell averaged current
 subplot(313)
 plot(midpoints,J_avg)
 ylabel("current")
 xlabel("midpoints")
-#yscale("log")
+title("Cell Averaged Current")
 
 #left exit bins
 figure(figsize = (5,10))
@@ -49,8 +128,12 @@ gsSol = zeros((11,2))
 gsSol[:,1] = -1*[0.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
 gsSol[:,2] = [0.58966,0.53112,0.44328,0.38031,0.33296,0.29609,0.26656,0.24239,
                                                         0.22223,0.20517,0.19055]
-plot(gsSol[:,1], gsSol[:,2],"+")
-plot(exit_left_bins[:,1], exit_left_bins[:,2])
+plot(gsSol[:,1], gsSol[:,2],"+",label="Garcia et al.")
+plot(exit_left_bins[:,1], exit_left_bins[:,2],label="QMC_sweep")
+xlabel("-mu")
+ylabel("Left Boundary - Angular Flux Dist.")
+title("Angular Flux Exit Distributions from Garcia et al., c = 1")
+legend()
 
 #right exit bins
 subplot(212)
@@ -58,5 +141,8 @@ gsSol = zeros((11,2))
 gsSol[:,1] = 1*[0.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
 gsSol[:,2] = [6.08E-06,6.93E-06,9.64E-06,1.62E-05,4.39E-05,1.69E-04,5.73E-04,
                                             1.51E-03,3.24E-03,5.96E-03,9.77E-03]
-plot(gsSol[:,1], gsSol[:,2],"+")
-plot(exit_right_bins[:,1], exit_right_bins[:,2])
+plot(gsSol[:,1], gsSol[:,2],"+", label="Garcia et al.")
+plot(exit_right_bins[:,1], exit_right_bins[:,2], label="QMC_sweep")
+xlabel("mu")
+ylabel("Right Boundary - Angular Flux Dist.")
+legend()
