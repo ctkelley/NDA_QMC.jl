fed6346837c4412d21caa749f3e8097185d69ba8
spasmann <spasmann@nd.edu>
2021-03-23 15:07:51 -0400

    Update qmc_init.jl

--- src/qmc_init.jl
+++ src/qmc_init.jl
@@ -15,7 +15,8 @@ function qmc_init(Nx, na2, s)
     fr(mu) = -sqrt(mu)
 
     #initialize sobol sequence
-    rng = SobolSeq(3)
+    rng = SobolSeq(2)
+
     if (N >0)
         skip(rng,N) #skipping the expected number is suggested for Sobol
     end
@@ -25,24 +26,19 @@ function qmc_init(Nx, na2, s)
         rng_bndl = SobolSeq(2)
         skip(rng_bndl,Nb) #skipping the expected number is suggested for Sobol
     end
-    rng_bndr = 0
-    if (has_right > 0)
-        rng_bndr = SobolSeq(2)
-        skip(rng_bndr,Nb) #skipping the expected number is suggested for Sobol
-    end
 
     #define angular flux mesh
     #exit_left_bins data structure to hold the exiting angular flux,
     #the first column has the bin centers and the
     #and the second holds the values.
     #exit_right_bins is the same
-    dmu = 1/nbins
+    dmu = 1/na2
     #right bins
-    exit_right_bins = zeros((nbins,2))
+    exit_right_bins = zeros((na2,2))
     exit_right_bins[:,1] = range(dmu/2, stop = 1- dmu/2, step = dmu)
     exit_right_bins[:,2] .= 0
     #left bins
-    exit_left_bins = zeros((nbins,2))
+    exit_left_bins = zeros((na2,2))
     exit_left_bins[:,1] = -1*range(dmu/2, stop = 1- dmu/2, step = dmu)
     exit_left_bins[:,2] .= 0
 
