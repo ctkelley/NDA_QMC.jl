6b7dc15171d26644723ed649c3fa486dd093f8bc
spasmann <46267220+spasmann@users.noreply.github.com>
2021-11-19 12:20:43 -0500

    Moved Cross Section Import to Init
    Moved the import of cross section data used in the multigroup problems to be within in the multiGroup_init function. Now the user simply specifies number of groups.

--- src/Sam_Original/qmc_init.jl
+++ src/Sam_Original/qmc_init.jl
@@ -1,3 +1,5 @@
+using DelimitedFiles
+using LinearAlgebra
 """
 The functions below used to precompute data for various problem types.
 """
@@ -65,7 +67,7 @@ function qmc_init(Geometry, generator, N, LB, RB, Nx, na2, s, sigs, sigt)
         elseif (Geometry == "Sphere")
             Geo = 3
         else
-            print("Geo must be: 'Slab', 'Cylinder', or 'Sphere'")
+            error("Geometry must be: 'Slab', 'Cylinder', or 'Sphere'")
         end
 
         qmc_data = (        Geo = Geo,
@@ -96,6 +98,113 @@ function qmc_init(Geometry, generator, N, LB, RB, Nx, na2, s, sigs, sigt)
                             high_edges = high_edges,
                             midpoints = midpoints,
                             edges = edges)
+        return qmc_data
+end
+
+"""
+    multiGroup_init(Geometry, generator, N, LB, RB, Nx, numGroups)
+Multi-Group initialization function. Solution data stored in ./HDPE_data for 12,
+70, and 618 groups.
+"""
+function multiGroup_init(Geometry, generator, N, LB, RB, Nx, numGroups)
+
+        if (numGroups != 12) && (numGroups != 70) && (numGroups != 618)
+            error("Number of groups must be 12, 70, or 618.")
+        end
+        D = readdlm(joinpath(@__DIR__, "./HDPE_data/D_$(numGroups)G_HDPE.csv"), ',', Float64)
+        siga = readdlm(joinpath(@__DIR__, "./HDPE_data/Siga_$(numGroups)G_HDPE.csv"), ',', Float64)
+        sigs = readdlm(joinpath(@__DIR__, "./HDPE_data/Scat_$(numGroups)G_HDPE.csv"), ',', Float64)
+        sigs = reverse(sigs, dims=2)
+        sigt = 1 ./ (3*D)
+
+        hasLeft = false
+        hasRight = false
+
+        dx = (RB-LB)/Nx
+        #define tally mesh
+        low_edges = range(LB, stop=RB-dx, length=Nx)
+        high_edges = low_edges.+dx
+        midpoints = 0.5*(high_edges + low_edges)
+        edges = range(LB, stop=RB, length=Nx+1)
+
+        #define angular flux mesh
+        dmu = 1/na2
+        #right bins
+        exit_right_bins = zeros((na2,2))
+        exit_right_bins[:,1] = range(dmu/2, stop = 1- dmu/2, step = dmu)
+        exit_right_bins[:,2] .= 0
+        #left bins
+        exit_left_bins = zeros((na2,2))
+        exit_left_bins[:,1] = -1*range(dmu/2, stop = 1- dmu/2, step = dmu)
+        exit_left_bins[:,2] .= 0
+
+        G = numGroups # number of groups
+        source_strength = 1.0
+        # analytic solution
+        Q = source_strength*ones(numGroups) # source
+        true_flux = inv(Diagonal(sigt[:,1]) .- sigs)*Q # returns diagonal matrix
+
+        temp1 = zeros(Nx, G)
+        for i in 1:G
+            temp1[:,i] = ones(Nx)*sigt[i]
+        end
+        sigt = temp1
+        source = source_strength*ones(Nx,G)
+
+        # phi_avg is defaulted to = zeros(Nx)
+        phi_edge = zeros(Nx+1,G)
+        phi_avg = source_strength*zeros(Nx,G)
+        # used for boundary sources
+        phi_left = 1.0  # source_strength
+        phi_right = 1.0 # source_strength
+
+        dphi = zeros(Nx,G)
+        phi_s = zeros(Nx,G) .+ 1e-6
+        # current
+        J_avg = zeros(Nx,G)
+        J_edge = zeros(Nx+1,G)
+        # Garcia parameter
+        c = s
+        # Geometry
+        if (Geometry == "Slab")
+            Geo = 1
+        elseif (Geometry == "Cylinder")
+            Geo = 2
+        elseif (Geometry == "Sphere")
+            Geo = 3
+        else
+            error("Geometry must be: 'Slab', 'Cylinder', or 'Sphere'")
+        end
+
+        qmc_data = (        Geo = Geo,
+                            N = N,
+                            Nx = Nx,
+                            G = G,
+                            RB = RB,
+                            LB = LB,
+                            dmu = dmu,
+                            exit_left_bins = exit_left_bins,
+                            exit_right_bins = exit_right_bins,
+                            phi_edge = phi_edge,
+                            phi_avg = phi_avg,
+                            phi_left = phi_left,
+                            phi_right = phi_right,
+                            dphi = dphi,
+                            phi_s = phi_s,
+                            J_avg = J_avg,
+                            J_edge = J_edge,
+                            sigt = sigt,
+                            sigs = sigs,
+                            source = source,
+                            c = c,
+                            generator = generator,
+                            hasRight = hasRight,
+                            hasLeft = hasLeft,
+                            low_edges = low_edges,
+                            high_edges = high_edges,
+                            midpoints = midpoints,
+                            edges = edges,
+                            true_flux = true_flux)
         return qmc_data
 end
 
@@ -159,7 +268,7 @@ function garcia_init(Geometry, generator, N, LB, RB, Nx, na2, s)
         elseif (Geometry == "Sphere")
             Geo = 3
         else
-            print("Geo must be: 'Slab', 'Cylinder', or 'Sphere'")
+            error("Geometry must be: 'Slab', 'Cylinder', or 'Sphere'")
         end
 
         phi_left = 1
@@ -257,7 +366,7 @@ function const_infMed_init(Geometry, generator, N, LB, RB, Nx, na2, s, sigs, sig
         elseif (Geometry == "Sphere")
             Geo = 3
         else
-            print("Geo must be: 'Slab', 'Cylinder', or 'Sphere'")
+            error("Geometry must be: 'Slab', 'Cylinder', or 'Sphere'")
         end
 
         # used for boundary sources
@@ -361,7 +470,7 @@ function linear_infMed_init(Geometry, generator, N, LB, RB, Nx, na2, s, sigs, si
         elseif (Geometry == "Sphere")
             Geo = 3
         else
-            print("Geo must be: 'Slab', 'Cylinder', or 'Sphere'")
+            error("Geometry must be: 'Slab', 'Cylinder', or 'Sphere'")
         end
 
         # used for boundary sources
@@ -468,7 +577,7 @@ function quadratic_infMed_init(Geometry, generator, N, LB, RB, Nx, na2, s, sigs,
         elseif (Geometry == "Sphere")
             Geo = 3
         else
-            print("Geo must be: 'Slab', 'Cylinder', or 'Sphere'")
+            error("Geometry must be: 'Slab', 'Cylinder', or 'Sphere'")
         end
 
         # used for boundary sources
--- src/Sam_Original/tests/multi_group.jl
+++ src/Sam_Original/tests/multi_group.jl
@@ -15,43 +15,27 @@ using Statistics
 pygui(true)
 
 ###############################################################################
-#### Import Cross Section Data
-###############################################################################
-
-D = readdlm(joinpath(@__DIR__, "../HDPE_data/D_12G_HDPE.csv"), ',', Float64)
-siga = readdlm(joinpath(@__DIR__, "../HDPE_data/Siga_12G_HDPE.csv"), ',', Float64)
-sigs = readdlm(joinpath(@__DIR__, "../HDPE_data/Scat_12G_HDPE.csv"), ',', Float64)
-sigs = reverse(sigs, dims=2)
-sigt = 1 ./ (3*D)
-
-###############################################################################
 #### Parameters
 ###############################################################################
 
 Nx = 50     # number of tally cells
 na2 = 11    # number of angles for angular mesh
 s = [1]     # parameter in Garcia/Siewert
-N = 2^11   # number of particles per itertion per source
+N = 2^11    # number of particles per itertion per source
 LB = 0      # left bound
-RB = 500      # right bound
+RB = 500    # right bound
+G = 12      # number of groups
 geometry = "Slab"
 generator = "Sobol"
 
-qmc_data = qmc_init(geometry, generator, N, LB, RB, Nx, na2, s, sigs, sigt)
+qmc_data = multiGroup_init(geometry, generator, N, LB, RB, Nx, G)
 phi_avg_Sobol, phi_edge, dphi, J_avg, J_edge, history, itt = qmc_source_iteration(s,qmc_data)
-
-###############################################################################
-#### Analytic Solution
-###############################################################################
-source_strength = 1.0
-G = size(sigt)[1] # number of groups
-Q = source_strength*ones(G) # source
-flux = inv(Diagonal(sigt[:,1]) .- sigs)*Q # returns diagonal matrix
 
 ###############################################################################
 #### Plots
 ###############################################################################
 midpoints = qmc_data.midpoints
+sol = qmc_data.true_flux
 
 figure()
 title("Scalar Flux")
@@ -73,7 +57,7 @@ color_sequence = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c",
 title("Group Scalar Flux")
 for i in 1:1:G
     plot(midpoints, phi_avg_Sobol[:,i], label=i,color=color_sequence[i] ) #color=color_sequence[i]
-    plot(midpoints,flux[i]*ones(Nx),"--",color=color_sequence[i] )
+    plot(midpoints,sol[i]*ones(Nx),"--",color=color_sequence[i] )
 end
 #plot(midpoints,phi_avg, label="QMC")
 ylabel("cell averaged flux")
